f76005fda04b66008ba558be2c206ec0b018ffb2
txx <txx@archlinux.com>
2026-09-07 15:49:30 +0800

    test paddleOCR

---
+++ script.py
@@ -0,0 +1,35 @@
+import os
+import sys
+import pymupdf
+#import paddleocr 
+#import pa
+from paddleocr import PaddleOCR
+
+ocr = PaddleOCR(
+	use_doc_orientation_classify=False,
+	use_doc_unwarping=False,
+	use_textline_orientation=False,
+	enable_mkldnn=False,
+	engine="paddle",
+)
+result = ocr.predict("./pic/IMG_20260830_234003.jpg")
+for res in result:
+	res.print()
+	res.save_to_img("output")
+	res.save_to_json("output")
+
+## print(dir(os))
+#print("Hello, Arch Linux")
+#print("Hello, Arch Linux")
+
+# doc = pymupdf.open("./materials/sample.pdf")
+#doc = pymupdf.open("./script.txt")
+#print(doc)
+##
+#out = open("output.txt", "wb")
+#for page in doc:
+#	text = page.get_text().encode("utf8")
+#	out.write(text)
+#	out.write(bytes((12,)))
+#out.close()
+#doc.close()
